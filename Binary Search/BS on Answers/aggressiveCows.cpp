cc49b909e9f4c41f6a88d06fcedf12cb2a4d251f
Diya Burman <diyaburman60@gmail.com>
2025-06-21 14:46:13 +0530

    commit

--- Binary Search/BS on Answers/aggressiveCows.cpp
+++ Binary Search/BS on Answers/aggressiveCows.cpp
@@ -3,37 +3,49 @@
 #include <algorithm>
 using namespace std;
 
-class Solution {
-  public:
-    bool canWePlace(vector<int> &stalls, int dist, int cows) {
-        int cntCows=1;
-        int last=stalls[0];
-        for(int i=1;i<stalls.size();i++) {
-            if(stalls[i]-last>=dist) {
+class Solution
+{
+public:
+    bool canWePlace(vector<int> &stalls, int dist, int cows)
+    {
+        int cntCows = 1;
+        int last = stalls[0];
+        for (int i = 1; i < stalls.size(); i++)
+        {
+            if (stalls[i] - last >= dist)
+            {
                 cntCows++;
-                last=stalls[i];
+                last = stalls[i];
             }
         }
-        if(cntCows>=cows) {
+        if (cntCows >= cows)
+        {
             return true;
-        } else {
+        }
+        else
+        {
             return false;
         }
     }
-    
-    int aggressiveCows(vector<int> &stalls, int k) {
-        sort(stalls.begin(),stalls.end());
-        int n=stalls.size();
-        int s=1;
-        int e=stalls[n-1]-stalls[0];
-        int ans=-1;
-        while(s<=e) {
-            int mid=(s+e)/2;
-            if(canWePlace(stalls,mid,k)==true) {
-                ans=mid;
-                s=mid+1;
-            } else {
-                e=mid-1;
+
+    int aggressiveCows(vector<int> &stalls, int k)
+    {
+        sort(stalls.begin(), stalls.end());
+        int n = stalls.size();
+        int s = 1;
+        int e = stalls[n - 1] - stalls[0];
+        int ans = -1;
+        while (s <= e)
+        {
+            int mid = (s + e) / 2;
+            if (canWePlace(stalls, mid, k) == true)
+            {
+                ans = mid;
+                s = mid + 1;
+            }
+            else
+            {
+                e = mid - 1;
             }
         }
         return ans;
