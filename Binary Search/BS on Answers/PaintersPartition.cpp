71966e32be029741c6b3cf6d98befc66890e1fa0
Diya Burman <diyaburman60@gmail.com>
2025-06-22 17:04:13 +0530

    commit

---
+++ Binary Search/BS on Answers/PaintersPartition.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<numeric>
+using namespace std;
+
+int func(vector<int> &arr, int pages) {
+    int student=1;
+    long long pagesStudent=0;
+    for(int i=0;i<arr.size();i++) {
+        if(pagesStudent+arr[i]<=pages) {
+            pagesStudent+=arr[i];
+        } else {
+        student++;
+        pagesStudent=arr[i];
+        }
+    }
+    return student;
+    }
+    
+int findPages(vector<int> &arr, int k) {
+    if(arr.size()<k) return -1;
+    int s=*max_element(arr.begin(),arr.end());
+    int e=accumulate(arr.begin(),arr.end(),0);
+    int ans=-1;
+    while(s<=e) {
+        int mid=(s+e)/2;
+        int cntStudents=func(arr,mid);
+        if(cntStudents>k) {
+            s=mid+1;
+        } else {
+            ans=mid;
+            e=mid-1;
+        }
+    }
+    return ans;
+}
+int findLargestMinDistance(vector<int> &boards, int k)
+{
+    return findPages(boards, k);
+}
