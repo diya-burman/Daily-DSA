a5815fb4f4a2a807434e13964ff4e922dce147a9
Diya Burman <diyaburman60@gmail.com>
2025-07-24 11:41:38 +0530

    commit

--- Binary Search Trees/Floor.cpp
+++ Binary Search Trees/Floor.cpp
@@ -9,23 +9,30 @@ struct Node
 };
 
 // Function to search a node in BST.
-class Solution {
+class Solution
+{
 
-  public:
-    int floor(Node* root, int x) {
-       int floor=-1;
-    while(root) {
-        if(root->data==x) {
-            floor=root->data;
-            return floor;
-        } 
-        if(root->data<x) {
-            floor=root->data;
-            root=root->right;
-        } else {
-            root=root->left;
+public:
+    int floor(Node *root, int x)
+    {
+        int floor = -1;
+        while (root)
+        {
+            if (root->data == x)
+            {
+                floor = root->data;
+                return floor;
+            }
+            if (root->data < x)
+            {
+                floor = root->data;
+                root = root->right;
+            }
+            else
+            {
+                root = root->left;
+            }
         }
-    }
-    return floor;
+        return floor;
     }
 };
