e13b5fe00a41c7d4611bc44aaa8ad3a242c6beda
Diya Burman <diyaburman60@gmail.com>
2025-08-01 11:22:44 +0530

    commit

---
+++ Recursion/CountTheSubsequenceWithSumK.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int printS(int ind, int s, int sum, int arr[], int n)
+{
+    if (ind == n)
+    {
+        // condition satisfied
+        if (s == sum) return 1;
+        
+        // condition not satisfied
+        else return 0;
+    }
+
+    s += arr[ind];
+
+    int l = printS(ind + 1, s, sum, arr, n);
+          
+    s-=arr[ind];
+
+    // not pick
+    int r = printS(ind + 1, s, sum, arr, n);
+
+    return l + r;
+}
+ 
+int main()
+{
+    int arr[] = {1, 2, 1};
+    int n = 3;
+    int sum = 2;
+    cout << printS(0, 0, sum, arr, n);
+    return 0;
+}
