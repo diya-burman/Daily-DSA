34aa3b7861965e111c52489c2cdc9208f699c3b1
Diya Burman <diyaburman60@gmail.com>
2025-09-22 10:35:57 +0530

    commit

--- Greedy Algorithms/NMeetingsIn1Room.cpp
+++ Greedy Algorithms/NMeetingsIn1Room.cpp
@@ -1,40 +1,48 @@
-#include<iostream>
-#include<vector>
-#include<algorithm>
+#include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
-class Solution {
-  public:
-    struct Meeting {
+class Solution
+{
+public:
+    struct Meeting
+    {
         int start;
         int end;
         int pos;
     };
-    
-    static bool comp(Meeting a, Meeting b) { // based on end time
-        if(a.end==b.end) return a.pos<b.pos;
-        return a.end<b.end;
+
+    static bool comp(Meeting a, Meeting b)
+    { // based on end time
+        if (a.end == b.end)
+            return a.pos < b.pos;
+        return a.end < b.end;
     }
-    
-    int maxMeetings(vector<int>& start, vector<int>& end) {
-        int n=start.size();
+
+    int maxMeetings(vector<int> &start, vector<int> &end)
+    {
+        int n = start.size();
         vector<Meeting> arr(n);
-        
-        for(int i=0;i<n;i++) {
-            arr[i].start=start[i];
-            arr[i].end=end[i];
-            arr[i].pos=i+1;
+
+        for (int i = 0; i < n; i++)
+        {
+            arr[i].start = start[i];
+            arr[i].end = end[i];
+            arr[i].pos = i + 1;
         }
-        
-        sort(arr.begin(),arr.end(),comp);
-        
-        int cnt=1;
-        int freeTime=arr[0].end;
-        
-        for(int i=1;i<n;i++) {
-            if(arr[i].start>freeTime) {
+
+        sort(arr.begin(), arr.end(), comp);
+
+        int cnt = 1;
+        int freeTime = arr[0].end;
+
+        for (int i = 1; i < n; i++)
+        {
+            if (arr[i].start > freeTime)
+            {
                 cnt++;
-                freeTime=arr[i].end;
+                freeTime = arr[i].end;
             }
         }
         return cnt;
