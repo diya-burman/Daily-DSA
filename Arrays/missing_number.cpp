add087f58306b6f60b2fc032678e4c5d62b30053
Diya Burman <diyaburman60@gmail.com>
2025-09-25 09:52:13 +0530

    commit

--- Arrays/missing_number.cpp
+++ Arrays/missing_number.cpp
@@ -4,28 +4,20 @@
 
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    int missingNumber(vector<int>& nums) {
+    int missingNumber(vector<int> &nums)
+    {
         int n = nums.size();
         sort(nums.begin(), nums.end());
-        for (int i = 0; i < n; i++) {
-            if (nums[i] != i) {
+        for (int i = 0; i < n; i++)
+        {
+            if (nums[i] != i)
+            {
                 return i;
             }
         }
         return n;
     }
 };
-
-int main() {
-    Solution solution;
-
-    // Sample test case
-    vector<int> nums = {3, 0, 1};
-    int missing = solution.missingNumber(nums);
-
-    cout << "Missing number: " << missing << endl;
-
-    return 0;
-}
